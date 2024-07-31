7f5d971c66175b0929e3ecb8d184e77e0eb88aaa
Md Shovo <shovom677@gmail.com>
2024-07-31 18:35:17 +0600

    setup dashbord and user role

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,34 @@
 <?php
 
+use App\Http\Controllers\Dashbord\DashbordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('frontend.contant.homepage');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
+Route::middleware(['auth'])->prefix('/admin')->controller(DashbordController::class)->name('dashbord')->group(function(){
+
+    Route::get('/dashboard',[DashbordController::class,'dashboard'])->name('admin');
+
+
+
+
+
+
+
 });
+
+
+
